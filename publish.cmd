054b1cebb3754c9c15fce51b7b521efea3271533
Hiroki Taniguchi <tani.hiroki@gmail.com>
2018-03-07 14:32:13 +0900

    initial code

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=jpbottani-Web-Deploy.pubxml -p:Password=3ku1xbKfwkfjWM5gEk9naqxfpTBw512CFSjDPvaZKd26Zs7stboQXcbRqja2
+
